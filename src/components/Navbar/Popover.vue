8d515d6858833d476b189a29c0c30aa2ea15ec03
chaitb <chaitwheels@gmail.com>
2021-03-08 18:07:41 +0530

    Fix mobile navigation

--- src/components/Navbar/Popover.vue
+++ src/components/Navbar/Popover.vue
@@ -1,18 +1,18 @@
 <template>
-    <section>
-        <a @click="toggle" v-click-outside="close" class="block py-1" style="cursor:pointer;">
+    <main v-click-outside="close">
+        <a @click="toggle" class="block py-1" style="cursor:pointer;">
             <slot name="link" />
         </a>
-        <nav :class="{hidden : isHidden}">
+        <nav :class="{hidden : isHidden}" @click="close">
             <div class="absolute left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
                 <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                     <div class="z-20 relative grid gap-6 bg-white dark:bg-darker-grey px-2 py-8 sm:gap-8 sm:px-4">
-                        <slot name="popover" />
+                        <slot name="popover"/>
                     </div>
                 </div>
             </div>
         </nav>
-    </section>
+    </main>
 </template>
 
 <script>
