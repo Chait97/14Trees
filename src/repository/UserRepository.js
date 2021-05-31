8c82334746d65482ae0e684f92ecbd0d00e55bfb
VaniKulkarni <vanikulkarnister@gmail.com>
2021-05-31 17:41:23 +0530

    Adding CRUD ops to users collection with sample user object

--- src/repository/UserRepository.js
+++ src/repository/UserRepository.js
@@ -1,4 +1,4 @@
-import db from "~/repository/db/firebaseInit";
+import db from "../repository/db/firebaseInit";
 import { collection, doc, setDoc, getDocs, query, where } from "firebase/firestore";
 
 function all(qSnap) {
@@ -9,6 +9,7 @@ function all(qSnap) {
 	});
 	return docs
 }
+
 export default {
 	async getCampaignDonors(campaign) {
 		const q = query(collection(db, "users"), where("campaign", "==", campaign));
@@ -20,4 +21,75 @@ export default {
 		const querySnapshot = await getDocs(q);
 		return all(querySnapshot);
 	},
+	async test()
+	{
+		const userRepository = Repository.get("user");
+        userRepository.get();
+        userRepository.add();
+        userRepository.update();
+	},
+  get() {
+    let users = [];
+    db.collection("users")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          users.push({
+            name: doc.data().name,
+            test: doc.data().test,
+          });
+          console.log(doc.id, " => ", doc.data());
+        });
+        return users;
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  },
+
+  add() {
+    var user1 = {
+      uid: "JR0Gg8l0uocFhimjkqoIg97T01j2",
+      name: "test 1",
+      email: "test@gmail.com",
+      panNumber: "AWPKUYH897T",
+      feedback: "Would like to visit the place, great initiative!!!",
+    };
+    db.collection("users")
+      .add(user1)
+      .then((docRef) => {
+        console.log("User added: ", docRef.id);
+       // this.$router.push("/");
+      })
+      .catch((error) => {
+        console.error("Error adding user: ", error);
+      });
+  },
+
+  update() {
+    let uidToUpdate = "JR0Gg8l0uocFhimjkqoIg97T01j2"; //this.$route.params.uidToUpdate
+    var user2 = {
+      panNumber: "2AWPKUYH897T",
+      feedback: "2Would like to visit the place, great initiative!!!",
+    };
+    db.collection("users")
+      .where("uid", "==", uidToUpdate)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          doc.ref
+            .update({
+              panNumber: user2.panNumber,
+              feedback: user2.feedback,
+            })
+            .then(() => {
+              console.log("User updated");
+              // this.$router.push({
+              //   name: "view-user",
+              //   params: { uid: uidToUpdate },
+              // });
+            });
+        });
+      });
+  },
 };
